c161d2162d49b252da96df088ff77292c8e98b4a
limajpp <joaopedrocal90@gmail.com>
2026-05-02 12:42:45 -0300

    feat: implement KNNClassifier with fit and predict methods

---
+++ models/knn.py
@@ -0,0 +1,36 @@
+import numpy as np
+from collections import Counter
+
+class KNNClassifier:
+    def __init__(self, k=3, distance_metric='euclidean'):
+        self.k = k
+        self.distance_metric = distance_metric
+        self.X_train = None
+        self.y_train = None
+
+    def fit(self, X, y):
+        self.X_train = X
+        self.y_train = y
+
+    def _compute_distances(self, x_test_sample):
+        if self.distance_metric == 'euclidean':
+            return np.sqrt(np.sum((self.X_train - x_test_sample) ** 2, axis=1))
+        elif self.distance_metric == 'manhattan':
+            return np.sum(np.abs(self.X_train - x_test_sample), axis=1)
+        else:
+            raise ValueError(f"Unknown distance metric: {self.distance_metric}")
+
+    def predict(self, X_test):
+        predictions = []
+        for x_test_sample in X_test:
+            distances = self._compute_distances(x_test_sample)
+            
+            k_indices = np.argsort(distances)[:self.k]
+            
+            k_nearest_labels = self.y_train[k_indices]
+            
+            most_common = np.bincount(k_nearest_labels.astype(int)).argmax()
+            predictions.append(most_common)
+            
+        return np.array(predictions)
+    
